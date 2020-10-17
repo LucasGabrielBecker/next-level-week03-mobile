9e5277261ef7352b43b7bb1252be1f1a4d59bb92
Lucas Lopes <lucasgabrielbec@gmail.com>
2020-10-16 22:20:52 -0300

    Falta apenas form criacao

---
+++ routes.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack'
+
+const {Navigator, Screen} = createStackNavigator();
+
+import OrphanagesMap from "./src/pages/OrphanagesMap";
+import OrphanageDetails from "./src/pages/OrphanageDetails";
+import OrphanageData from "./src/pages/CreateOrphanage/OrphanageData";
+import SelectMapPosition from "./src/pages/CreateOrphanage/SelectMapPosition";
+import Header from './src/components/Header';
+
+export default function Routes(){
+    return(
+        <NavigationContainer>
+            <Navigator screenOptions={{ headerShown:false, cardStyle:{backgroundColor: "#f2f3f5"} }}>
+                <Screen
+                    name="OrphanagesMap"
+                    component={OrphanagesMap}
+                />
+
+                <Screen
+                    name="OrphanageDetails"
+                    component={OrphanageDetails}
+                    options={{
+                        headerShown:true,
+                        header: ()=> <Header showCancel={false} title="Orfanato"/>
+                    }}
+                />
+                <Screen
+                    name="SelectMapPosition"
+                    component={SelectMapPosition}
+                    options={{
+                        headerShown:true,
+                        header: ()=> <Header title="Selecione local no mapa"/>
+                    }}
+                    />
+                    <Screen name="OrphanageData"
+                        component={OrphanageData}
+                        options={{
+                            headerShown:true,
+                            header: ()=> <Header title="Informe os dados"/>
+                        }}
+                    />
+            </Navigator>
+        </NavigationContainer>
+    )
+};
